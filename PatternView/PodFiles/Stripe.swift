b31f5be7d9357142e84c4f4b321e980d19e34e4c
kysomers <kyledouglassomers@gmail.com>
2017-07-19 07:35:37 -0700

    added stripe class

---
+++ PatternView/PodFiles/Stripe.swift
@@ -0,0 +1,9 @@
+//
+//  Stripe.swift
+//  PatternView
+//
+//  Created by Kyle Somers on 7/19/17.
+//  Copyright © 2017 Kyle Somers. All rights reserved.
+//
+
+import Foundation
---
+++ PatternView/StripeView.swift
@@ -0,0 +1,21 @@
+//
+//  StripeView.swift
+//  PatternView
+//
+//  Created by Kyle Somers on 7/19/17.
+//  Copyright © 2017 Kyle Somers. All rights reserved.
+//
+
+import UIKit
+
+class StripeView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
