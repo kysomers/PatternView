ca694a70d13a920a5a228fc22cd45ca4e2a5e871
kysomers <kyledouglassomers@gmail.com>
2017-08-01 07:47:55 -0700

    took out an incomplete class

--- PatternView/PodFiles/RandomPolkaDotView.swift
+++ PatternView/PodFiles/RandomPolkaDotView.swift
@@ -6,27 +6,29 @@
 //  Copyright © 2017 Kyle Somers. All rights reserved.
 //
 
-import UIKit
+//Making this for the next update
 
-class RandomPolkaDotView: UIView {
-    
-    //different sizes of polka dots : small, medium, large, and mixed
-    //different amounts of polks dots: few, some, lots
-    //background color
-    //dot color
-    //random seed
-    
-    func setup(polkaDotAmount : PolkaDotAmount, polkaDotSize : PolkaDotSize, backgroundColor : UIColor, dotColor : UIColor, randomSeed : Int?){
-        
-        self.clipsToBounds = true
-        if let randomSeed = randomSeed{
-            srand48(randomSeed)
-
-        }
-        
-        
-    }
-        
-
-
-}
+//import UIKit
+//
+//class RandomPolkaDotView: UIView {
+//    
+//    //different sizes of polka dots : small, medium, large, and mixed
+//    //different amounts of polks dots: few, some, lots
+//    //background color
+//    //dot color
+//    //random seed
+//    
+//    func setup(polkaDotAmount : PolkaDotAmount, polkaDotSize : PolkaDotSize, backgroundColor : UIColor, dotColor : UIColor, randomSeed : Int?){
+//        
+//        self.clipsToBounds = true
+//        if let randomSeed = randomSeed{
+//            srand48(randomSeed)
+//
+//        }
+//        
+//        
+//    }
+//        
+//
+//
+//}
